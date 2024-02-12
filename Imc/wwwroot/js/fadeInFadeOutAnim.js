cee57f9a335efde041b5bf65df558447502d2984
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-12 12:52:24 -0300

    melhorando estrutura do projeto

--- Imc/wwwroot/js/fadeInFadeOutAnim.js
+++ Imc/wwwroot/js/fadeInFadeOutAnim.js
@@ -16,16 +16,3 @@ window.fadeOutAndShowMainContent = function () {
         }, tempoFadeOut);
     });
 };
-
-window.checkingForms = function () {
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-    let notRobot = document.getElementById("notRobot").checked;
-
-    if (email.trim() === "" || senha.trim() === "" || !notRobot) {
-        alert("Por favor, preencha todos os campos.");
-    }
-    else {
-        window.location.href = '/pages/index.html'
-    }
-}
