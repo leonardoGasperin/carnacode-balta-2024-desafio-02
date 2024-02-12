5379f42929f281de94ae1ebfe214f9bf026e664a
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-12 12:52:44 -0300

    refatorando forms checkers

---
+++ Imc/wwwroot/js/formsCheck.js
@@ -0,0 +1,12 @@
+window.checkingLoginForms = function () {
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let notRobot = document.getElementById("notRobot").checked;
+
+    if (email.trim() === "" || senha.trim() === "" || !notRobot) {
+        alert("Por favor, preencha todos os campos.");
+    }
+    else {
+        window.location.href = '/calculadora'
+    }
+}
